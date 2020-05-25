dca8d94dfae664d896ec67710dca373564476761
Drake24 <zjhassler@gmail.com>
2020-05-25 11:15:42 -0500

    initial set up and made seed data

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Todd",
+    "photo":"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    "scores":[3,4,3,1,5,3,5,4,3,2]
+  },
+  {
+    "name":"Beth",
+    "photo":"https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "scores":[4,4,4,2,3,3,4,2,1,5]
+  },
+  {
+    "name":"Ron",
+    "photo":"https://i.chzbgr.com/full/9327633408/hB844F8AB/ron-swanson-meme-about-big-ben-the-clocktower-in-london",
+    "scores":[5,5,5,5,5,5,5,5,5,5]
+  },
+  {
+    "name":"Leslie",
+    "photo":"https://i.insider.com/5824aa9046e27a1c008b5eec?width=300&format=jpeg&auto=webp",
+    "scores":[1,1,1,1,1,1,1,1,1,1]
+  }
+]
